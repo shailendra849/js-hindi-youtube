8171fc9a07d04ebb92b7644eea395994ca5e8f15
shailendra849 <shailendrad699@gmail.com>
2025-04-28 10:55:48 +0000

    variable and dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountID = 1444553
+let accountEmail = "shailendra@gmail.com"
+var accountPassword = "12345"
+accountcity ="jaipur"
+
+// acountID =2 // not allowed
+
+
+accountEmail="shailendrad445@gamil.com"
+accountPassword="223334455"
+accountcity= "bhopal"
+
+console.log(accountID);
+
+
+console.table([accountEmail,accountID,accountPassword,accountcity])
