5176d24643493c03429c7d359532c193666751ed
shailendra849 <shailendrad699@gmail.com>
2025-04-30 07:45:30 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3+3) we are using nodejs, not browser
+
+console.log( 3
+    +3) // code shound be readability should be high
+     console.log( "shailendra")
+
+
+
+
+     let name = "hitesh"
+     let age = 18
+     let isLoggedIn = false
+     let state;
+
+
+
+     //number => 2 to power 53
+    //  BigInt
+    //  String => ""
+    //  boolean =>true/false
+     // null => standalone value
+     // undefined =>
+    // symbol +> unique
+
+
+
+
+    // Object
+
+    console.log(typeof "hitesh")
+    console.log(typeof age)
+    
