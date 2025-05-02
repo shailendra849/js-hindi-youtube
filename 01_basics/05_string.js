dc5a08b67e944aade083cfaea27f2dc2803bceec
shailendra849 <shailendrad699@gmail.com>
2025-05-02 07:28:59 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,4 @@
+ const name = "hitesh"
+ const repocount = 50
+ console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+ 
---
+++ 01_basics/datatypeSummery.js
@@ -0,0 +1,18 @@
+// stack (primitive), heap (non-primitive)
+let myYoutubename = "hiteshchoudharydotcom"
+ let anothername = myYoutubename
+ anothername= "chaiaurcode"
+
+ console.log(myYoutubename);
+ console.log( anothername);
+  let userOne = {
+    email : "user@googlee.com",
+    upiId :userl
+  }
+   
+let usertwo =  userOne
+
+usertwo.email = "shailendra@gmail.com"
+
+console.log(userOne.email);
+console.log(usertwo.email);
