cd639029bb9a83644c1c6826576d35fa31b7653c
shailendra849 <shailendrad699@gmail.com>
2025-05-04 17:46:16 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -48,11 +48,11 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+ console.log(Object.keys(tinderUser));
+ console.log(Object.values(tinderUser));
+ console.log(Object.entries(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+ console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 const course = {
@@ -62,7 +62,7 @@ const course = {
 }
 
 // course.courseInstructor
-
+// const {courseInstructor} = course
 const {courseInstructor: instructor} = course
 
 // console.log(courseInstructor);
